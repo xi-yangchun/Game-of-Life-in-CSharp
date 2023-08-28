c81beb6d0fde5aa7861e80cbfdbd5911723c6393
xi-yangchub <ittetsu1945@gmail.com>
2023-08-28 16:53:01 +0900

    再コミット LifeGame本体とForm1を分離した。

--- gameoflife/Form1.cs
+++ gameoflife/Form1.cs
@@ -8,14 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace gameoflife
 {
     public partial class Form1 : Form
     {
-        int[,] state;
-        int[,] num_life;
         int h;int w;
-        Random rand;
+        GoLModel GOL;
         Rectangle[,] pix;
         int pixsize;
         Timer timer1;
@@ -29,10 +28,9 @@ namespace gameoflife
             ControlStyles.AllPaintingInWmPaint, true
             );
 
-            rand = new Random();
+            
             h = 200;w = 200;
-            state = new int[h,w];
-            num_life = new int[h,w];
+            GOL = new GoLModel(h, w);
             pix = new Rectangle[h,w];
             pixsize = 3;
 
@@ -42,22 +40,6 @@ namespace gameoflife
                 for (int j = 0; j < w; j++)
                 {
                     pix[i, j] = new Rectangle(j * ps, i * ps, ps, ps);
-                }
-            }
-
-            for (int i = 0; i < h; i++)
-            {
-                for(int j=0; j < w; j++)
-                {
-                    state[i, j] = rand.Next(2);
-                }
-            }
-
-            for (int i = 0; i < h; i++)
-            {
-                for (int j = 0; j < w; j++)
-                {
-                    num_life[i, j] = 0;
                 }
             }
 
@@ -75,58 +57,9 @@ namespace gameoflife
 
         }
 
-        private void Update_Life() {
-            //Console.WriteLine("this is unko." + state[49,48].ToString());
-            for (int i= 0; i < h;i++){
-                for(int j= 0; j < w; j++){
-                    for(int dy = -1; dy <= 1; dy++){
-                        for(int dx= -1; dx <= 1; dx++){
-                            int y = i + dy;
-                            int x = j+ dx;
-                            if (x < 0) { x = x + w ; }
-                            else if (x > w - 1) { x = x - w ; }
-                            if (y < 0) { y = y + h; }
-                            else if (y > h - 1) { y = y - h; }
-                            if (dx!=0 || dy!=0) {
-                                if (state[y, x] == 1) {
-                                    num_life[i, j] += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < h; i++)
-            {
-                for (int j = 0; j < w; j++)
-                {
-                    if (state[i, j] == 0 && num_life[i, j] == 3)
-                    {
-                        state[i, j] = 1;
-                    }
-                    else if (state[i, j] == 1 && num_life[i, j] == 2)
-                    {
-                        state[i, j] = 1;
-                    }
-                    else if (state[i, j] == 1 && num_life[i, j] == 3)
-                    {
-                        state[i, j] = 1;
-                    }
-                    else { state[i, j] = 0; }
-                }
-            }
-
-            for (int i = 0; i < h; i++){
-                for (int j = 0; j < w; j++){
-                    num_life[i,j] = 0;
-                }
-            }
-        }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-            this.Update_Life();
+            GOL.Update_Life();
             this.Invalidate();
         }
 
@@ -141,11 +74,12 @@ namespace gameoflife
             brush.Dispose();
             */
             SolidBrush brush1 = new SolidBrush(Color.Black);
+            int[,] arr = GOL.get_state();
             for (int i = 0; i < h; i++)
             {
                 for(int j=0;j < w; j++)
                 {
-                    if(state[i, j] == 1)
+                    if(arr[i, j] == 1)
                     {
                         g.FillRectangle(brush1, pix[i, j]);
                     }
---
+++ gameoflife/GoLModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace gameoflife
+{
+    public partial class GoLModel
+    {
+        int h;int w;
+        int[,] state; int[,] num_life;
+        Random rand;
+        public GoLModel(int h,int w) { 
+            rand= new Random();
+            this.h=h;this.w=w;
+            num_life = new int[h, w];
+            state = new int[h, w];
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    state[i, j] = rand.Next(2);
+                }
+            }
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    num_life[i, j] = 0;
+                }
+            }
+
+        }
+
+        public void Update_Life()
+        {
+            //Console.WriteLine("this is unko." + state[49,48].ToString());
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        for (int dx = -1; dx <= 1; dx++)
+                        {
+                            int y = i + dy;
+                            int x = j + dx;
+                            if (x < 0) { x = x + w; }
+                            else if (x > w - 1) { x = x - w; }
+                            if (y < 0) { y = y + h; }
+                            else if (y > h - 1) { y = y - h; }
+                            if (dx != 0 || dy != 0)
+                            {
+                                if (state[y, x] == 1)
+                                {
+                                    num_life[i, j] += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    if (state[i, j] == 0 && num_life[i, j] == 3)
+                    {
+                        state[i, j] = 1;
+                    }
+                    else if (state[i, j] == 1 && num_life[i, j] == 2)
+                    {
+                        state[i, j] = 1;
+                    }
+                    else if (state[i, j] == 1 && num_life[i, j] == 3)
+                    {
+                        state[i, j] = 1;
+                    }
+                    else { state[i, j] = 0; }
+                }
+            }
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    num_life[i, j] = 0;
+                }
+            }
+        }
+
+        public int[,] get_state()
+        {
+            return this.state;
+        }
+    }
+}
